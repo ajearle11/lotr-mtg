71e9d95c60a9a87327a5d956caab96427b29a461
ajearle11 <ajearle9@gmail.com>
2023-09-14 16:36:48 +0100

    Started logic for multiclick

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -4,15 +4,25 @@ import { addToString } from "../../helperFunctions/helperFunctions";
 import "./index.css";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { useAppContext } from "../../contexts/";
 
 const Card: React.FC<{ card: ApiResponseData; hasGot: boolean }> = ({
   card,
   hasGot,
 }) => {
   const navigate = useNavigate();
+  const { cards } = useAppContext();
+
+  //some logic for multiclicking
+  const handleMultiClick = (e: React.MouseEvent<HTMLDivElement>): void => {
+    const target = e.target as HTMLDivElement;
+    if (target.classList.contains("card")) {
+      target.classList.toggle("selected");
+    }
+  };
 
   return (
-    <div className="card">
+    <div onClick={handleMultiClick} className="card">
       <h3>{card.name}</h3>
       <div className="name-container">
         <p>#{addToString(card.id.toString())}</p>
