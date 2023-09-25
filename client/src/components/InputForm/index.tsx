847104c02e24c4167bc876dbc4ae75cb0036af6b
ajearle11 <58401691+ajearle11@users.noreply.github.com>
2023-09-25 14:14:46 +0100

    Fixed issue with username text focus

--- client/src/components/InputForm/index.tsx
+++ client/src/components/InputForm/index.tsx
@@ -3,11 +3,20 @@ import { FormProps } from "../../interfaces/";
 import "./index.css";
 
 const InputForm = ({ onSubmit, handleEmail, handlePassword }: FormProps) => {
+  const checkUsernameNodes = () => {
+    let usernameField = document.querySelectorAll(".input-container label")[0] as HTMLElement;
+    let usernameFieldFocus = document.querySelectorAll(".input-container input")[0] as HTMLElement;
+    usernameField.style.top = '-12px'
+    usernameField.style.fontSize = '12px'
+    console.log(document.hasFocus())
+    usernameFieldFocus.focus()
+  };
+
   return (
     <form onSubmit={onSubmit}>
       <div className="input-container">
         <input type="mail" onChange={handleEmail} required />
-        <label>Username</label>
+        <label onClick={checkUsernameNodes}>Username</label>
       </div>
       <div className="input-container">
         <input type="password" onChange={handlePassword} required />
