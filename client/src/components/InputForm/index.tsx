4c7c56ae9ab4ebf0fd6c1666758df0dfd4cdeba7
ajearle11 <ajearle9@gmail.com>
2023-09-29 12:24:44 +0100

    Fixed fields

--- client/src/components/InputForm/index.tsx
+++ client/src/components/InputForm/index.tsx
@@ -3,39 +3,54 @@ import { FormProps } from "../../interfaces/";
 import "./index.css";
 
 const InputForm = ({ onSubmit, handleEmail, handlePassword }: FormProps) => {
-  
-  
-  document.addEventListener('click', (e) => {
-    let usernameField = document.querySelectorAll(".input-container label")[0] as HTMLElement;
-    let usernameFieldFocus = document.querySelectorAll(".input-container input")[0] as HTMLElement;
+  document.addEventListener("click", (e) => {
+    let usernameField = document.querySelectorAll(
+      ".input-container label"
+    )[0] as HTMLElement;
+    let usernameFieldFocus = document.querySelectorAll(
+      ".input-container input"
+    )[0] as HTMLElement;
+    let usernameFieldValue = document.querySelectorAll(
+      ".input-container input"
+    )[0] as HTMLInputElement;
     if (e.target === usernameField || e.target === usernameFieldFocus) {
-      usernameField.style.top = '-12px'
-      usernameField.style.fontSize = '12px'
-      usernameFieldFocus.focus()
-    } else {
-      if (document.activeElement === usernameFieldFocus || document.activeElement === usernameField)
-      usernameField.style.position = 'absolute'
-      usernameField.style.top = '0px'
-      usernameField.style.fontSize = '16px'
-      usernameFieldFocus.blur()
-    }
-  })
+      usernameField.style.top = "-12px";
+      usernameField.style.fontSize = "12px";
 
-  document.addEventListener('click', (e) => {
-    let passwordField = document.querySelectorAll(".input-container label")[1] as HTMLElement;
-    let passwordFieldFocus = document.querySelectorAll(".input-container input")[1] as HTMLElement;
-    if (e.target === passwordField || e.target === passwordFieldFocus) {
-      passwordField.style.top = '-12px'
-      passwordField.style.fontSize = '12px'
-      passwordFieldFocus.focus()
+      usernameFieldFocus.focus();
     } else {
-      if (document.activeElement === passwordFieldFocus || document.activeElement === passwordField)
-      passwordField.style.position = 'absolute'
-      passwordField.style.top = '0px'
-      passwordField.style.fontSize = '16px'
-      passwordFieldFocus.blur()
+      if (usernameFieldValue.value === "") {
+        usernameField.style.position = "absolute";
+        usernameField.style.top = "0px";
+        usernameField.style.fontSize = "16px";
+      }
+      usernameFieldFocus.blur();
     }
-  })
+  });
+
+  document.addEventListener("click", (e) => {
+    let passwordField = document.querySelectorAll(
+      ".input-container label"
+    )[1] as HTMLElement;
+    let passwordFieldFocus = document.querySelectorAll(
+      ".input-container input"
+    )[1] as HTMLElement;
+    let passwordFieldValue = document.querySelectorAll(
+      ".input-container input"
+    )[1] as HTMLInputElement;
+    if (e.target === passwordField || e.target === passwordFieldFocus) {
+      passwordField.style.top = "-12px";
+      passwordField.style.fontSize = "12px";
+      passwordFieldFocus.focus();
+    } else {
+      if (passwordFieldValue.value === "") {
+        passwordField.style.position = "absolute";
+        passwordField.style.top = "0px";
+        passwordField.style.fontSize = "16px";
+      }
+      passwordFieldFocus.blur();
+    }
+  });
 
   return (
     <form onSubmit={onSubmit}>
